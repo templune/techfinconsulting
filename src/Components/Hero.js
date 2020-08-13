5af849967cfc126f1fd35e29c0bf99ebda3200ca
templune <templune@gmail.com>
2020-08-12 19:16:30 -0500

    Add bootstrap and reactstrap

---
+++ src/Components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+class Hero extends React.Component {
+    render() {
+      return (
+       
+    
+      <div className="container">
+          <h1>Somos una consultora de tecnología integral</h1>
+          <p>Creamos soluciones que mejoran las cosas para las organizaciones con las que trabajamos. Ayudamos a nuestros clientes a mejorar todo, desde la eficiencia hasta los ingresos, el compromiso del personal y la lealtad de los clientes.</p>
+      </div>
+      )
+    }
+  }
+
+export default Hero;
+
